bd409a08d497d68785dbaf9d3b8e0197d51c0d9f
htk291 <htk291@gmail.com>
2018-05-13 11:39:24 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './src/App';  // このファイルパスを編集
+
+AppRegistry.registerComponent('ReactNativeTypescriptSetup', () => App);
---
+++ rn-cli.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  getSourceExts () {
+    return ['ts', 'tsx']
+  },
+
+  getTransformModulePath () {
+    return require.resolve('react-native-typescript-transformer')
+  }
+}
